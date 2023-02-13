544157d50020794b042af7c757c59d2117dc8fd5
YAChen123 <andychen123583@gmail.com>
2023-02-13 13:11:09 -0500

    add /time path

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from datetime import datetime
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello world!'
+
+@app.route('/time')
+def curr_time():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S") 
+    print("Current Time =", current_time)
 
 
 app.run(host='0.0.0.0',
